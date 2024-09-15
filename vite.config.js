8dc57475af5636af51b5d834399327abd794eac5
MatacheAlexandru <111695999+MatacheAlexandru@users.noreply.github.com>
2024-09-15 16:28:01 +0300

    t

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "/goit-react-hw-05-movies/", // numele repo-ului tău GitHub
+  base: "/goit-react-hw-05-movies/", // Acesta asigură că toate rutele sunt relative la acest subdirector.
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: "assets/[name].js",
+        chunkFileNames: "assets/[name].js",
+        assetFileNames: "assets/[name].[ext]",
+      },
+    },
+  },
 });
